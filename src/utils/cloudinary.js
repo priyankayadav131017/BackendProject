f7db141cce838b01e0eeba7d258b8d6fd72469e5
priyankayadav131017 <priyankayadav131017@gmail.com>
2026-08-04 13:55:47 +0530

    coverImage changes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,24 +1,25 @@
 import {v2 as cloudinary} from "cloudinary"
 import fs  from "fs"
 
-cloudinary.config({ 
+const uploadOnCloudinary=async(localFilePath)=>{
+    cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 }); 
-
-const uploadOnCloudinary=async(localFilePath)=>{
     try {
         if(!localFilePath) return null
         //upload the file on cloudinary
-         const response=await cloadinary.uploader.upload(localFilePath,{
+         const response=await cloudinary.uploader.upload(localFilePath,{
             resource_type:"auto"
         })
         //file has been uploaded successfully
-         console.log("file is uploaded on cloudinary",response.url);
-         return response 
+        // console.log("file is uploaded on cloudinary",response.url);
+        fs.unlinkSync(localFilePath)
+        return response 
     } catch (error) {
         fs.unlinkSync(localFilePath)//remove the locally saved temporary file as the upload operation got failed
+        return null;
     }
 }
 
